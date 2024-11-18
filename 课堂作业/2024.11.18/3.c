2fef91351f87857f0e64dda9786021d9c2c1e096
zhangshaoxuan70 <105834641+zhangshaoxuan70@users.noreply.github.com>
2024-11-18 11:29:27 +0800

    Upload via files

---
+++ 课堂作业/2024.11.18/3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+	int n,i,j;
+	scanf("%d",&n);
+	for(i=n/2+1;i>=1;i--)
+	{
+		for(j=1;j<=n;j++)
+		{
+			if(i>j || n-j+1<i)
+				printf(" ");
+			else
+				printf("*");
+		}
+		printf("\n");
+	}
+	for(i=1;i<=n/2+1;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			if(i>=j || n-j<i)
+				printf(" ");
+			else
+				printf("*");
+		}
+		printf("\n");
+	}
+}
